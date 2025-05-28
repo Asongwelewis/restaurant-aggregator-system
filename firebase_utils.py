02fe7b4004a3d51f7fae565e0c9f6b02390887da
Mael-14 <robertmael143@gmail.com>
2025-05-28 05:07:37 +0200

    edits

--- firebase_utils.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, auth
-
-cred = credentials.Certificate("firebase_Key.json")
-firebase_admin.initialize_app(cred)
-
-def set_user_role(uid: str, role: str):
-    auth.set_custom_user_claims(uid, {"role": role})
