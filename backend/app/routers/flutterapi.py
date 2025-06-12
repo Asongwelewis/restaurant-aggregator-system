9c4cd0c9a2ec1ba8a6324f6010bee30b091bce8b
Nathanaelkam <kamkamdem.nathanael@ictuniversity.edu.cm>
2025-06-12 02:45:13 +0100

    Update flutterapi.py

--- backend/app/routers/flutterapi.py
+++ backend/app/routers/flutterapi.py
@@ -2,29 +2,30 @@ from app.models import flutterpay_request
 from app.helper_function import initialise_payment
 from fastapi import FastAPI
 
-app = FastAPI(    
-    description= "REstaurant Aggregator System API",
-    tittle = "Restaurant Aggregator system",
-    docs_url="/"
-    )
 
-@app.post("/flutterwave/pay/")
-async def pay_now(payment:flutterpay_request):
+flutter_router = APIRouter(
+    prefix="/flutterwave",
+    tags=["flutterwave"]
+)
+
+@flutter_router.post("/pay/")
+async def pay_now(payment: flutterpay_request):
     result = await initialise_payment(
-        email = payment.email,
-        amount= payment.amount,
-        phone= payment.phone,
-        name = payment.name,
-        redirect_url= payment.redirect_url
-   ) 
+        email=payment.email,
+        amount=payment.amount,
+        phone=payment.phone,
+        name=payment.name,
+        redirect_url=payment.redirect_url
+    )
     if result["status"] == "success":
-        return{
-            "message":"payment link created",
+        return {
+            "message": "payment link created",
             "payment_link": result["data"]["link"]
         }
     else:
-        return{
-            "message":"something when wrong",
-            "error": result.get("message","Unknown error")
+        return {
+            "message": "something went wrong",
+            "error": result.get("message", "Unknown error")
         }
+ 
     
