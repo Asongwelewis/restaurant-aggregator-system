96a534c75754b3d0a8533ce293d652c89e117851
Mael-14 <robertmael143@gmail.com>
2025-06-12 20:05:00 +0200

    adding backend to main

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,33 @@
 from fastapi import FastAPI
-from app.routers import restaurants, ratings, search  
+from dotenv import load_dotenv
+import os
+import firebase_admin
+from firebase_admin import credentials
+import cloudinary
+from models import UserRegister, UserLogin, EmailRequest, RefreshTokenRequest, NotificationToken
 
-app = FastAPI()
+load_dotenv()
 
-app.include_router(restaurants.router, prefix="/api/restaurants")
-app.include_router(ratings.router, prefix="/api/ratings")
-app.include_router(search.router, prefix="/api/search")
+# Initialize Firebase Admin SDK
+if not firebase_admin._apps:
+    cred = credentials.Certificate("ras-3854a-firebase-adminsdk-fbsvc-909b3d4db8.json")
+    firebase_admin.initialize_app(cred, {
+        "databaseURL": "https://ras-3854a-default-rtdb.firebaseio.com/" #os.getenv("FIREBASE_DB_URL")
+    })
 
-@app.get("/")
-def read_root():
-    return {"message": "welcome to odis the best app for your food "}
+# Initialize Cloudinary
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
+
+app = FastAPI(
+    description="Restaurant Aggregator System API",
+    title="Restaurant Aggregator System",
+    docs_url="/"
+)
+
+from routes import router
+app.include_router(router)
+# app.include_router(router, prefix="/routes", tags=["Routes"])
